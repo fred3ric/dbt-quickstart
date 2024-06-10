f558606c83a3c6dc4f2dde64dfff983b42c3fd49
frederic reinier <frederic.reinier@sonergia.fr>
2024-06-10 14:46:18 +0000

    add test

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,17 @@
+with source as (
+    select * 
+    from {{ source('jaffle_shop','customers')}}
+
+),
+staged as (
+
 select
     id as customer_id,
     first_name,
     last_name
 
-from  {{ source('jaffle_shop','customers')}}
+from  source
+)
+
+select *
+from staged
